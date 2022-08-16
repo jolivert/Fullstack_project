91a8bf93b836fdefbdbf8ac02e744ee5c9487ea5
Marina Font <marinafoper.1998@gmail.com>
2022-08-16 19:01:31 +0200

    Project page layout with the projects list

---
+++ FRONTEND/src/components/CreateProject.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import '../assets/style/header.css'
+
+
+const CreateProject = () => {
+  return (
+    <div>
+     <h3>New project</h3>
+     <button>+ add</button>
+    </div>
+  )
+}
+
+export default CreateProject
--- FRONTEND/src/pages/Projects.jsx
+++ FRONTEND/src/pages/Projects.jsx
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import Header from '../components/Header.jsx'
+import CreateProject from '../components/CreateProject.jsx'
 import '../assets/style/projects.css'
 
 const Projects = () => {
-    return (
-      <div>
-        <Header/>
-        <p>Soy la pagina de tu lista de proyectos</p>
-        <p>fsdklgjds</p>
-      </div>
-    )
+ 
+  const ReadProjects = () => {
+    const info = [
+      {
+        title: "Project 1",
+        description: "Fullstack project based on scrum and planning poket methodologies",
+        poname: "Jose"
+      },
+      {
+        title: "Project 2",
+        description: "Fullstack project based on scrum and planning poket methodologies",
+        poname: "Davina"
+      },
+      {
+        title: "Project 3",
+        description: "Fullstack project based on scrum and planning poket methodologies",
+        poname: "Gerard"
+      }
+    ]
+    return info;
   }
-  export default Projects
+
+  const [data, setData] = useState(ReadProjects);
+
+
+  return (
+    <div>
+      <Header/>
+      <div class="projectpage">
+        <div class="create">
+          <CreateProject/>+
+        </div>
+        <div class="list">
+          <ul>
+            {data.map(item=> (
+              <div id="listitem">
+                <div>
+                  <h3>{item.title}</h3>
+                  <p id="po" >{item.poname} </p>
+                </div>
+                <p>{item.description} </p>
+                <img class="icon" src="https://img.icons8.com/FE4B2B/delete"/>    
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Projects
