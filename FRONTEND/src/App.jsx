65203a93ebb015a4c98e994dab2512e2fade12df
Davina-dev <davinamr91@gmail.com>
2022-08-24 08:51:54 +0200

    solucionado problema docker-compose y pagina inicialpara probar

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import Login from "./components/Login.jsx";
 import TodoTasks from "./pages/TodoTasks.jsx";
 import Tasks from "./pages/Tasks.jsx";
+import Initial from "./pages/Initial.jsx";
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
@@ -27,11 +28,8 @@ function App() {
 
     if (toggle=== "login") {return <Login onLogin={login} ontoggle={togfunct}/>}
     else{ return <SignUp/>
-  
       
     };
-    
-    
     
 
 
@@ -40,9 +38,10 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<PlanningPocker />} />
+            <Route index element={<Initial />} />
             <Route path="TodoTasks" element={<TodoTasks/>} />
             <Route path="Tasks" element={<Tasks />} />
+            <Route path="PlanningPocker" element={< PlanningPocker />} />
           </Route>
         </Routes>
       </div>
